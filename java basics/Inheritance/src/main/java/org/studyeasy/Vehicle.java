0fcffe8d6e4cf20f14b419d5e4a81ad302a155e8
regiin09 <regiinarulrithik@gmail.com>
2025-03-23 09:51:23 +0530

    first commi

---
+++ java basics/Inheritance/src/main/java/org/studyeasy/Vehicle.java
@@ -0,0 +1,9 @@
+package org.studyeasy;
+
+public class Vehicle {
+    public String Engine;
+    public int wheels;
+    public int seats;
+    public int fuelTank;
+    public String lights;
+}
--- java basics/Inheritance/src/main/java/org/studyeasy/bike.java
+++
@@ -1,4 +0,0 @@
-package org.studyeasy;
-
-public class bike {
-}
